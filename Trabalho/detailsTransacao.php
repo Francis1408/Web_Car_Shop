a5a7b31800b2d43ad7429c27fa5ff26eb4f14a31
F-SpaceMan <leonardodiasbicalho@gmail.com>
2022-12-02 22:18:28 -0300

    aaaaaaaaaaa

--- Trabalho/detailsTransacao.php
+++ Trabalho/detailsTransacao.php
@@ -70,39 +70,46 @@
 	$row = oci_fetch_array($s, OCI_BOTH);
 
 
+	echo "<div class='container-cat'";
 	echo "<h3> Data Publicação: " . $row[4] . "</h3>";
-	echo "<h3> Data da Venda: " . $row[1] . "</h3> <br>";
-	echo "<h3> Numero do Anuncio: " . $row[3] . "</h3> <br>";
-	echo "<h3> Valor da Venda R$: " . $row[0] . "</h3> <br>";
-	echo "<h3> Descrição: <br> " . $row[2] . "</h3> <br>";
+	echo "<h3> Data da Venda: " . $row[1] . "</h3> ";
+	echo "<h3> Numero do Anuncio: " . $row[3] . "</h3> ";
+	echo "<h3> Valor da Venda R$: " . $row[0] . "</h3> ";
+	echo "<h3> Descrição:  " . $row[2] . "</h3> ";
+	echo "</div>";
 
-
-	echo "<h2> Detalhes do Veículo </h2> <br>";
+	echo "<div class='container-cat'";
+	echo "<h2> Detalhes do Veículo </h2> ";
 	echo "<h3> Placa do Carro: " . $row[11] . "</h3>";
-	echo "<h3> Modelo: " . $row[12] . "</h3> <br>";
-	echo "<h3> Ano: " . $row[13] . "</h3> <br>";
-	echo "<h3> Cor: " . $row[14] . "</h3> <br>";
-	echo "<h3> Marca: " . $row[15] . "</h3> <br>";
+	echo "<h3> Modelo: " . $row[12] . "</h3> ";
+	echo "<h3> Ano: " . $row[13] . "</h3> ";
+	echo "<h3> Cor: " . $row[14] . "</h3> ";
+	echo "<h3> Marca: " . $row[15] . "</h3> ";
+	echo "</div>";
 
+	echo "<div class='container-cat'";
 	if (isset($_GET['detalhesTransacao1'])) {
-		echo "<h2> Detalhes do Comprador </h2> <br>";
+		echo "<h2> Detalhes do Comprador </h2> ";
 	} else {
-		echo "<h2> Detalhes do Vendedor </h2> <br>";
+		echo "<h2> Detalhes do Vendedor </h2> ";
 	}
 
-	echo "<h3> Nome: " . $row[10] . "</h3> <br>";
-	echo "<h3> Email: " . $row[8] . "</h3> <br>";
-	echo "<h3> Telefone: " . $row[9] . "</h3> <br>";
+	
+	echo "<h3> Nome: " . $row[10] . "</h3> ";
+	echo "<h3> Email: " . $row[8] . "</h3> ";
+	echo "<h3> Telefone: " . $row[9] . "</h3> ";
+	echo "</div>";
 
+	echo "<div class='container-cat'";
 	echo "<h2> FeedBack da Transação </h2>";
 
 	if ($row[7]) {
-		echo "<h2> Positiva </h2> <br>";
+		echo "<h2> Positiva </h2> ";
 	} else {
-		echo "<h2> Negativa </h2> <br>";
+		echo "<h2> Negativa </h2> ";
 	}
-	echo "<h3>Comentários: <br>" . $row[6] . "</h3>";
-
+	echo "<h3>Comentários: " . $row[6] . "</h3>";
+	echo "</div>";
 
 	?>
 
