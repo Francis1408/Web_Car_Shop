ae268796f31747ed82dd45092d321c75256c435d
Francisco Abreu Gonçalves <71090685+Francis1408@users.noreply.github.com>
2022-12-02 22:51:50 -0300

    css

--- Trabalho/compraPage.php
+++ Trabalho/compraPage.php
@@ -7,43 +7,57 @@
 
 ?>
 
-
 <!DOCTYPE html>
 <html>
 <head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>COMPRAR</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>COMPRAR</title>
 </head>
 <body>
+    <div class="container-inicial">
+        <div class="form-cad">
+            <form method="post">
+                <div class="form-header">
+                    <h1> Preencha os dados de pagamento </h1>
+                </div>
+                <div class="input-group" style="color:white">
+                    <div class="input-box">
+                        <h3>Bandeira do cartão</h3>
+                        <br>
+                        <label for="master">MasterCard</label><br>
+                        <input type="radio"  id="master" name="cartao" Value="MasterCard">
+                        <label for="visa">Visa</label><br>
+                        <input type="radio" id="visa" name="cartao" Value="Visa">
+                        <label for="elo">Elo</label><br>
+                        <input type="radio" id="elo" name="cartao" Value="Elo">
+                        <label for="hiper">HiperCard</label><br>
+                        <input type="radio" id="hiper" name="cartao" Value="Hipercard">
+                    </div>
 
-	 <h3> Preencha os dados de pagamento </h3>
-    <form method="post">
-    	<h4>Bandeira do cartão</h4>
-        <input type="radio" id="master" name="cartao" Value="MasterCard">
-        <label for="master">MasterCard</label><br>
-        <input type="radio" id="visa" name="cartao" Value="Visa">
-        <label for="visa">Visa</label><br>
-        <input type="radio" id="elo" name="cartao" Value="Elo">
-        <label for="elo">Elo</label><br>
-        <input type="radio" id="hiper" name="cartao" Value="Hipercard">
-        <label for="hiper">HiperCard</label><br>
+                    <div class="input-box">
+                        <label for="numero">Número do cartão</label><br>
+                        <input type="text" size="50" id="numero" name="numero">
+                    </div>
 
-        <h4>Número do cartão</h4>
-        <label for="numero">Número</label><br>
-        <input type="text" size="50" id="numero" name="numero">
+                   <div class="input-box">
+                        <label for="titular">Titular</label><br>
+                        <input type="text" size="100" id="titular" name="titular">
+                    </div>
 
-        <h4>Titular</h4>
-        <label for="titular">Titular</label><br>
-        <input type="text" size="100" id="titular" name="titular">
-
-        <h4>CVV</h4>
-        <label for="cvv">CVV</label><br>
-        <input type="text" size="10" id="cvv" name="cvv"><br>
-
-  
-        <input type="submit" name="compra" value="Comprar">
-    </form>
-
+                    <div class="input-box">
+                        <label for="cvv">CVV</label><br>
+                        <input type="text" size="10" id="cvv" name="cvv"><br>
+                    </div>
+                    <div class="login-button">
+                        <button type="submit" name="compra">
+                            Comprar
+                        </button>
+                    </div>
+                </div>
+            </form>
+        </div>    
+    </div>
 </body>
 </html>
+
